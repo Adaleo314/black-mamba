3cb687b1ff4231ea6b8ff06b913462d3b7e55784
Adam Leonard <115667156+Adaleo314@users.noreply.github.com>
2023-01-03 12:59:47 -0500

    Update variations.py

--- variations.py
+++ variations.py
@@ -4,7 +4,7 @@
 
 message = input("Enter a message: ")
 
-print("Lowecase:", message.lower())
+print("Lowercase:", message.lower())
 print("Uppercase:", message.upper())
 print("Capitalized:", message.capitalize())
 print("Title Case", message.title())
