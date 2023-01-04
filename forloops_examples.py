2f4d5fa845c5fafa6115e7181492b14a05f436cc
Adam Leonard <acleonard2@gmail.com>
2023-01-04 03:24:59 +0000

    more updates

--- forloops_examples.py
+++ forloops_examples.py
@@ -28,4 +28,50 @@ strings = "Happy Birthday"
 for x in strings:
     print(x)
 
+
+#nesting loops and conditionals
+#counter is less than 25 & divisible by 4, print it, otherwise add 1 until 
+# it equals 25
+counter = 1
+while counter <= 25:
+    if counter % 4 == 0:
+        print(counter)
+    counter += 1
+    
+#break and continue with loops
+#break will stop the loop
+#continue will skip the current iteration of the loop
+#this will count 3,5,7,9
+count = 0
+while count < 10:
+    if count % 2 == 0:
+        count += 1
+        continue
+    print(f"We're counting odd numbers: {count}")
+    count += 1
+    
+#break will stop entire loop
+#this will only count to 1 
+count = 1
+while count < 10:
+    if count % 2 == 0:
+        break
+    print(f"We're counting numbers: {count}")
+    count += 1
+    
+#else with loops
+for i in [1, 2, 3, 4, 5,]:
+    print(i)
+else:
+    print("For loop completed")  
+    
+#break in loop    
+
+colors = ['red', 'pink', 'blue,', 'orange', 'green']
+for color in colors:
+   if color == 'orange':
+       print("Orange is in the list")
+       break
+else:
+    print("Orange is not in the list")\
     
