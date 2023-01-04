f5ebbbe6f11f680e9dfa5a40ace4ae96c04db813
Adam Leonard <acleonard2@gmail.com>
2023-01-04 02:53:03 +0000

    updates

--- forloops_examples.py
+++ forloops_examples.py
@@ -1,5 +1,6 @@
 
-#Python Loops
+#Python For Loops
+#For Loops count from beginning of index to end
 
 #while statements
 count = 1
@@ -14,3 +15,17 @@ colors = ['blue', 'green', 'red', 'purple']
 
 for color in colors:
     print(color)
+    
+#for loops in dictionaries    
+ages = {'adam':35, 'mike':28, 'bill':17}
+for key in ages:
+    print(key)
+for key, value in ages.items():
+    print(key, value)
+
+#for loop with a string
+strings = "Happy Birthday"
+for x in strings:
+    print(x)
+
+    
