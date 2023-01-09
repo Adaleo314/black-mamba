cb820f06c81dd7a306b10e4de0fc6cdededfda28
Adam Leonard <acleonard2@gmail.com>
2023-01-09 03:47:00 +0000

    create file

---
+++ variabledatapractice.py
@@ -0,0 +1,12 @@
+#using the format function
+
+first_name = "John"
+last_name = "Doe"
+
+print("My first name is {}. My family name is {}".format(first_name, last_name))
+
+#f strings examples
+
+firstname = "Jane"
+lastname = "Doe"
+print(f"My name is {firstname}. My family name is {lastname}.")
