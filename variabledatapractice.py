a036f08a78471bc7af0bcbb4ff07a937e731592b
Adam Leonard <acleonard2@gmail.com>
2023-01-09 04:18:48 +0000

    practice

--- variabledatapractice.py
+++ variabledatapractice.py
@@ -10,3 +10,22 @@ print("My first name is {}. My family name is {}".format(first_name, last_name))
 firstname = "Jane"
 lastname = "Doe"
 print(f"My name is {firstname}. My family name is {lastname}.")
+
+#Dictionaries
+user = {"name": "John"}
+print(user)
+user["lastname"] = "Smith"
+
+print(user)
+
+#del user "" to delete users or values
+
+#Lists, sort and reverse can be used to edit list
+
+to_do_list = ['exercise', 'study', 'sleep']
+print(to_do_list)
+print(to_do_list[2])
+to_do_list.append('eat')
+list.remove('sleep')    
+
+
